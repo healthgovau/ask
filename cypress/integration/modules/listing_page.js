142e88cb2508b047100f0f3c93660da2d0eb2294
Hugo <hugo.heuman@health.gov.au>
2021-04-01 07:19:30 +0800

    [audio content type] Adjusted tests

--- cypress/integration/modules/listing_page.js
+++ cypress/integration/modules/listing_page.js
@@ -11,57 +11,42 @@ const tests = (cy) => {
       cy.visit(landingPageUri);
     });
 
-    /**
-     * @todo There is currectly a bug in Listing content page functionality.
-     * Test has been temporarily disabled until it has been resolved.
-     * @see https://trello.com/c/Hs667ytd/323-incorrect-results-count-on-sample-listing-page
-     */
-     it("renders correctly", () => {
-       // Take a snapshot for visual diffing.
-       const percyOptions = {};
-       cy
-         .visit(landingPageUri)
-         .scrollTo("bottom")
-         .get(".image img")
-         .each(($image, index, $images) => {
-           cy
-             .wrap($image)
-             .scrollIntoView()
-             .should("be.visible")
-         })
-         .then(() => {
-           cy.percySnapshot("health_starter_kit_sample_listing_page", percyOptions);
-         });
-     });
+    it("renders correctly", () => {
+      // Take a snapshot for visual diffing.
+      const percyOptions = {};
+      cy
+        .visit(landingPageUri)
+        .scrollTo("bottom")
+        .get(".image img")
+        .each(($image, index, $images) => {
+          cy
+            .wrap($image)
+            .scrollIntoView()
+            .should("be.visible")
+        })
+        .then(() => {
+          cy.percySnapshot("health_starter_kit_sample_listing_page", percyOptions);
+        });
+    });
 
-    /**
-     * @todo There is currectly a bug in Listing content page functionality.
-     * Test has been temporarily disabled until it has been resolved.
-     * @see https://trello.com/c/Hs667ytd/323-incorrect-results-count-on-sample-listing-page
-     */
-     it("correct result count is displayed", () => {
-       cy
-         .visit(landingPageUri)
-         .get("header")
-         .contains("6 results");
-     });
+    it("correct result count is displayed", () => {
+      cy
+        .visit(landingPageUri)
+        .get("header")
+        .contains("6 results");
+    });
 
-    /**
-     * @todo There is currectly a bug in Listing content page functionality.
-     * Test has been temporarily disabled until it has been resolved.
-     * @see https:trello.com/c/Hs667ytd/323-incorrect-results-count-on-sample-listing-page
-     */
-     it("facets display correctly", () => {
-       const facetId = "#facet_h_content_type";
+    it("facets display correctly", () => {
+      const facetId = "#facet_h_content_type";
 
-       cy
-         .visit(landingPageUri)
-         .get(facetId)
-         .should("be.visible")
-         .contains(facetId, "App or tool (1)")
-         .contains(facetId, "Publication (4)")
-         .contains(facetId, "Video (1)");
-     });
+      cy
+        .visit(landingPageUri)
+        .get(facetId)
+        .should("be.visible")
+        .contains(facetId, "App or tool (1)")
+        .contains(facetId, "Publication (4)")
+        .contains(facetId, "Video (1)");
+    });
   });
 }
 
