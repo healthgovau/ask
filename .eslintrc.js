d8c0d1f90311d2a1574f056df899ba59ad2274d0
Hugo <hugo.heuman@health.gov.au>
2021-04-09 06:55:24 +0800

    [linting] Linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,32 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    'airbnb-base',
-  ],
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  rules: {
-  },
+    "env": {
+        "browser": true,
+        "es2020": true
+    },
+    "extends": [
+        "standard"
+    ],
+    "parserOptions": {
+        "ecmaVersion": 11,
+        "sourceType": "module"
+    },
+    "rules": {
+      "no-mixed-operators": 0,
+      "no-return-assign": 0,
+      "new-cap": 0,
+      "no-undef": 0,
+      "camelcase": 0,
+      "no-var": 0,
+      "no-use-before-define": 0,
+      "no-unused-expressions": 0,
+      "no-useless-escape": 0,
+      "no-prototype-builtins": 0,
+      "no-sequences": 0,
+      "prefer-regex-literals": 0,
+      "eqeqeq": 0,
+      "valid-typeof": 0,
+      "prefer-const": 0,
+      "no-void": 0,
+      "brace-style": 0
+    }
 };
