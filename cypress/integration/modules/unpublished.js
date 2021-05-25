708b8f34b42410f81074ef7d368cc724cef8d459
Hugo <hugo.heuman@health.gov.au>
2021-05-25 15:20:57 +0800

    [unpublished] Added additional tests.

--- cypress/integration/modules/unpublished.js
+++ cypress/integration/modules/unpublished.js
@@ -6,26 +6,39 @@
 const {User} = require("./includes/User");
 
 const tests = (cy) => {
-const page = "/unpublished-content";
+  const page = "/resources/unpublished-content";
+  const refpage = "/reference-paragraphs";
 
-describe("The unpublished page.", () => {
-  it("Not visible when unauthenticated.", () => {
-    cy.request({url: page, failOnStatusCode: false}).its('status').should('eq', 403)
-      .visit('/')
-      .get('nav ul li.not-published').should('not.exist');
+  describe("The unpublished page.", () => {
+    it("Not visible when unauthenticated.", () => {
+      cy.request({url: page, failOnStatusCode: false}).its('status').should('eq', 403)
+        .visit('/')
+        .get('nav ul li.not-published').should('not.exist');
+    });
+
+    it("Visible when logged in", () => {
+      const user = new User();
+      const percyOptions = {};
+      user.login("author", "author");
+
+      cy.request({url: page, failOnStatusCode: false}).its('status').should('eq', 200)
+        .visit(page)
+        .get('nav ul li.not-published').should('exist')
+        .percySnapshot("health_starter_kit_sample_unpublished_page", percyOptions);
+    });
+
+    it("Appear in list when logged in", () => {
+      const user = new User();
+      const percyOptions = {};
+      user.login("author", "author");
+
+      cy.visit(refpage)
+        .get('nav ul li.not-published').should('exist')
+        .percySnapshot("health_starter_kit_sample_unpublished_list", percyOptions);
+    });
+
+
   });
-
-  it("Visible when logged in", () => {
-    const user = new User();
-    const percyOptions = {};
-    user.login("author", "author");
-
-    cy.request({url: page, failOnStatusCode: false}).its('status').should('eq', 200)
-      .visit(page)
-      .get('nav ul li.not-published').should('exist')
-      .percySnapshot("health_starter_kit_sample_unpublished_page", percyOptions);
-  });
-});
 }
 
 exports.tests = tests;
