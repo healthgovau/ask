c8e5a9eebc8540197bd18735b9e13706431ed4f5
Hugo <hugo.heuman@health.gov.au>
2021-06-01 11:01:53 +0800

    [unpublished] Added tests for listings but not for resources as its not working.

--- cypress/integration/modules/unpublished.js
+++ cypress/integration/modules/unpublished.js
@@ -8,6 +8,7 @@ const {User} = require("./includes/User");
 const tests = (cy) => {
   const page = "/resources/unpublished-content";
   const refpage = "/reference-paragraphs";
+  const resources = "/resources";
 
   describe("Unpublished pages.", () => {
     it("Not visible to unauthenticated users.", () => {
@@ -27,6 +28,15 @@ const tests = (cy) => {
         .percySnapshot("health_starter_kit_sample_unpublished_page", percyOptions);
     });
 
+    it("Does not appear in listing when list is viewed by unauthenticated user.", () => {
+      const percyOptions = {};
+
+      cy.visit(refpage)
+        .get('nav ul li.not-published').should('not.exist')
+        .percySnapshot("health_starter_kit_sample_unpublished_list", percyOptions);
+    });
+
+
     it("Appears in listing when list is viewed by authenticated user.", () => {
       const user = new User();
       const percyOptions = {};
@@ -36,6 +46,27 @@ const tests = (cy) => {
         .get('nav ul li.not-published').should('exist')
         .percySnapshot("health_starter_kit_sample_unpublished_list", percyOptions);
     });
+
+    it("Does not appear in resources when resources is viewed by unauthenticated user.", () => {
+      const user = new User();
+      const percyOptions = {};
+
+      cy.visit(resources)
+        .get('nav ul li.not-published').should('not.exist')
+        .percySnapshot("health_starter_kit_sample_unpublished_resources_unauthenticated", percyOptions);
+    });
+
+
+    //Currently not working. Leaving the test here for the future
+    // it("Appears in resources when list is viewed by authenticated user.", () => {
+    //   const user = new User();
+    //   const percyOptions = {};
+    //   user.login("author", "author");
+    //
+    //   cy.visit(resources)
+    //     .get('.health-listing .not-published').should('exist')
+    //     .percySnapshot("health_starter_kit_sample_unpublished_resources", percyOptions);
+    // });
 
 
   });
